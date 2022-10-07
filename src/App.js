f5e96964ba1e938ea94695f7d8f5b8580b6d9602
Alex <lex.courtney18@gmail.com>
2022-10-07 11:51:17 -0500

    create boiler plate about me component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Nav from './components/Navigation';
 import ContactForm from './components/Contact';
 import Project from './components/Project';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div style={{ width: "100%" }}>
-      <Header></Header>
+      <Header
+      contactSelected={contactSelected}
+      setContactSelected={setContactSelected}
+      ></Header>
       <main style={{ backgroundColor: "0b2545" }}>
         <div>
           <ContactForm></ContactForm>
