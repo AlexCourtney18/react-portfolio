73d991985079fd138116ba4a2b3d02a5df45d250
Alex <lex.courtney18@gmail.com>
2022-10-11 13:50:11 -0500

    add buttons to footer for github linkedin and stackoverlow

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,10 +4,27 @@ import React from 'react';
 
 function Footer() {
 
+    const openSite = url => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+    };
 
     return (
-        <footer className='container-fluid' style={{ background: "#8DA9c4" }}>
+        <footer className='flex-row px-1' style={{ background: "#8DA9c4" }}>
             <h1 style={{ color: "#0B2545" }}>Alex Courtney</h1>
+            <nav>
+            <ul className='flex-row'>
+                <li className='mx-2'>
+                <button onClick={() => openSite('https://github.com/AlexCourtney18')}>GitHub Repository</button>
+                </li>
+                <li className='mx-2'>
+                <button onClick={() => openSite('https://www.linkedin.com/in/alex-courtney-b90106164/')}>LinkedIn Profile</button>
+                </li>
+                <li className='mx-2'>
+                <button onClick={() => openSite('https://stackoverflow.com/')}>Stack Overflow</button>
+                </li>
+            </ul>
+            </nav>
+
         </footer>
     )
 }
