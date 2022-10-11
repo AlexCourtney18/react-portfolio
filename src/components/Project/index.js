5e9279efafebe20bcf44eb34ec32d92f338ec1d2
Alex <lex.courtney18@gmail.com>
2022-10-11 13:31:56 -0500

    add functionality to project buttons to open repo and deployed app on click

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,28 +5,48 @@ function Project({ title }) {
 
     const [projects] = useState([
         {
-            name: 'Homebase'
+            name: 'Homebase',
+            repo: 'https://github.com/AlexCourtney18/homebase',
+            deployed: 'https://enigmatic-bastion-69025.herokuapp.com/'
         },
         {
-            name: 'Doggypedia'
+            name: 'Doggypedia',
+            repo: 'https://github.com/AlexCourtney18/cocktail-doggo',
+            deployed: 'https://alexcourtney18.github.io/cocktail-doggo/'
         },
         {
-            name: 'Tech Blog'
+            name: 'Tech Blog',
+            repo: 'https://github.com/AlexCourtney18/tech-blog',
+            deployed: 'https://protected-sea-60494.herokuapp.com/'
         },
         {
-            name: 'Note Taker'
+            name: 'Note Taker',
+            repo: 'https://github.com/AlexCourtney18/note-taker',
+            deployed: 'https://intense-fortress-40157.herokuapp.com/'
         },
         {
-            name: 'Team Profile Generator'
+            name: 'Text Editor',
+            repo: 'https://github.com/AlexCourtney18/text-editor',
+            deployed: 'https://nameless-journey-88318.herokuapp.com/'
         },
         {
-            name: 'Weather Man'
+            name: 'Weather Man',
+            repo: 'https://github.com/AlexCourtney18/weather-man',
+            deployed: 'https://alexcourtney18.github.io/weather-man/'
         },
     ]);
 
+    const openRepo = url => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+    };
+
+    const openDeployed = url => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+    };
+
     //const currentProjects = projects.filter((project) => project.name === title);
     const [currentProject, setCurrentProject] = useState();
-    // console.log(projects);
+     console.log(projects);
 
     return (
         <div>
@@ -36,12 +56,13 @@ function Project({ title }) {
             <div className='flex-row'>
                 {projects.map((image, i) => (
                     <div className='img-thumbnail mx-1' key={i}>
+                    <h3>{image.name}</h3>
                     <img
                         src={require(`../../assets/projects/${i}.png`)}
                         alt={image.name}
                     />
-                    <button>GitHub Repository</button>
-                    <button className='flex-end'>Deployed Application</button>
+                    <button onClick={() => openRepo(`${image.repo}`)}>GitHub Repository</button>
+                    <button onClick={() => openDeployed(`${image.deployed}`)}>Deployed Application</button>
                     </div>
                 ))}
             </div>
