ea64783d896cbb3d214452ffc3e1151001b772a9
Alex <lex.courtney18@gmail.com>
2023-01-09 08:49:55 -0600

    updated screenshot of resume and changed description in About Me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,8 +7,8 @@ function About() {
         <h1 id="about">Who am I?</h1>
         <img src={headShot} className="my-2" alt="headshot" />
         <h3>I graduated from UW-Milwaukee in 2014 with a Bachelors degree in business administration. Since graduating I worked in customer facing sales roles for a few different
-            organizations. In April 2022 I decided to change fields to web development. I am currently enrolled in a Full-Stack Development coding bootcamp through University of Wisconsin.
-            I most enjoy working on the back-end functionality of applications.
+            organizations. In April 2022 I decided to change fields to web development. I have recently completed a Full-Stack Development coding bootcamp through University of Wisconsin
+            in November, 2022. I most enjoy working on the back-end functionality of applications.
         </h3>
         </section>
     );
